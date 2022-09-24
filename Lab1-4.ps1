5f4a20ded2b5491358add0686f22f1e4af52dcfb
mancholm <mancholm@sheridancollege.ca>
2022-09-24 17:47:48 -0400

    fisrt Change

---
+++ Lab1-4.ps1
@@ -0,0 +1,22 @@
+$PSVersionTable
+Write-Host “Hello World!” 
+Write-Host “I love you so Much!!” 
+Get-ExecutionPolicy -List 
+Get-Date
+Get-Date -format hh:mm:ss
+Function get-time
+{ 
+  get-date -format hh:mm
+  }  
+ get-time
+ Get-Module -ListAvailable 
+ Get-Command
+ Get-Command -Name *IP* 
+ Get-Command -Module NetTCPIP -Name *IP*
+
+ Start-Process notepad.exe
+ $NotepadProc = Get-Process -Name notepad 
+ $NotepadProc.WaitForExit()
+ Start-Process calc.exe
+ Get-Process | Get-Member 
+ Get-Date
